71132d82da4516219e9035cc352c20a682cd6f80
Ade Ahmad Faisal <107249140+adeahmadfaisal@users.noreply.github.com>
2022-09-24 20:05:35 +0700

    Add files via upload
    clean variabel

--- bubblesort.js
+++ bubblesort.js
@@ -1,26 +1,34 @@
 
 
-var arr = [234, 43, 55, 63,  5, 6, 235, 547];
+var arrDataSort= [234, 43, 55, 63,  5, 6, 235, 547];
 
-    for(var i = 0; i < arr.length; i++){
-       
-       
-      for(var j = 0; j < ( arr.length - i -1 ); j++){
-         
-        // Statement if Jika nilai variable lebih besar daripada nilai didepannya
-        
-        if(arr[j] > arr[j+1]){
-           
-          // Jika kondisi benar maka ubah
-          var temp = arr[j]
-          arr[j] = arr[j + 1]
-          arr[j+1] = temp
-        }
-      }
-    }
-    
-    console.log(arr);
    
-    
+ 
+  for (let index = 0; index < (arrDataSort.length-1); index++) {
+        
+      
+        for (let indexData = 0; indexData < (arrDataSort.length-1); indexData++) {
+            //Perulangan pertama untuk looping data untuk membandingkan antara perulangan pertama dan kedua
+                        
+
+            if (arrDataSort[indexData] > arrDataSort[indexData+1]) {
+                /* Statement if Jika angka pertama lebih besar daripada angka selanjutnya
+                 Jika kondisi benar maka ubah posisi number */
+
+                let temp = arrDataSort[indexData+1];
+                arrDataSort[indexData+1] = arrDataSort[indexData];
+                arrDataSort[indexData] = temp;
+            }
+            
+        }
+        
+    }
+
+   for (let x in arrDataSort)  {
+        //Show data sorting array
+
+        const sortingResult = arrDataSort[x];
+        console.log(sortingResult);
+   }
 
 
