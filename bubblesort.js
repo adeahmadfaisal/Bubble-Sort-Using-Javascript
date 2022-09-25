32830c0a0ed4ca3a039296eee92e8731326eae96
Ade Ahmad Faisal <107249140+adeahmadfaisal@users.noreply.github.com>
2022-09-25 18:58:07 +0700

    Update bubblesort.js
    use function

--- bubblesort.js
+++ bubblesort.js
@@ -1,19 +1,23 @@
 let numberArray = [234, 43, 55, 63, 5, 6, 235, 547];
+console.log(`Data before sorting ${numberArray}`);
 
-for (let index = 0; index < numberArray.length - 1; index++) {
-  for (let itemIndex = 0; itemIndex < numberArray.length - 1; itemIndex++) {
-    // The first loop for looping data to compare between
-    // the first and second iterations
+function sortBubble() {
+  for (let index = 0; index < numberArray.length - 1; index++) {
+    for (let itemIndex = 0; itemIndex < numberArray.length - 1; itemIndex++) {
+      // The first loop for looping data to compare between
+      // the first and second iterations
 
-    if (numberArray[itemIndex] > numberArray[itemIndex + 1]) {
-      // If function If the first number is greater than the next number
-      // If the condition is true then change the position of number
-      let tempNumber = numberArray[itemIndex + 1];
-      numberArray[itemIndex + 1] = numberArray[itemIndex];
-      numberArray[itemIndex] = tempNumber;
+      if (numberArray[itemIndex] > numberArray[itemIndex + 1]) {
+        // If function If the first number is greater than the next number
+        // If the condition is true then change the position of number
+        let tempNumber = numberArray[itemIndex + 1];
+        numberArray[itemIndex + 1] = numberArray[itemIndex];
+        numberArray[itemIndex] = tempNumber;
+      }
     }
   }
+  const showResultSorting = numberArray;
+  console.log(`Data after sorting ${showResultSorting}`);
 }
 
-console.log("Input", numberArray);
-console.log("Output", numberArray);
+sortBubble();
